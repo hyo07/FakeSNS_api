27a6c2333eb1c8b1230795fefa6cab28995563b4
hyo07 <yutaka727kato@gmail.com>
2019-03-14 21:31:31 +0900

    Update:test

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,26 +1,89 @@
 from django.test import TestCase
 from django.shortcuts import resolve_url
 from django.contrib.auth.models import User
+from app.models import Article, Profile, Like
+from django.utils import timezone
+
+
+class SignUpTest(TestCase):
+    def test_SignUpView(self):
+        # アカウント作成テスト
+        data = {
+            'username': 'hogetaro',
+            'password1': 'pas19240ls',
+            'password2': 'pas19240ls'
+        }
+        response = self.client.post(resolve_url('accounts:signup'), data)
+        self.assertRedirects(response, resolve_url("login"))
+        self.client.login(username="hogetaro", password="pas19240ls")
+        response = self.client.get(resolve_url("app:index"))
+        self.assertEqual(200, response.status_code)
 
 
 class ViewTest(TestCase):
 
     def setUp(self):
+        # ログイン設定
         self.user = User.objects.create_user(username='TestUser', password='passoworld')
         self.client.login(username='TestUser', password='passoworld')
 
     def test_UserDetail(self):
-        response = self.client.get(resolve_url('accounts:user_detail'))
+        # アクセスの確認
+        response = self.client.get(resolve_url('accounts:user_detail', pk=self.user.id))
         self.assertEqual(200, response.status_code)
+        # ユーザー情報作成
+        Profile.objects.create(introduction="create", sex=1, user_id=self.user.id)
+        response = self.client.get(resolve_url('accounts:user_update', pk=self.user.id))
+        self.assertEqual(200, response.status_code)
+        # ユーザー情報更新
+        profile = Profile.objects.get(user_id=self.user.id)
+        profile.introduction = "update"
+        profile.save()
+        response = self.client.get(resolve_url('accounts:user_update', pk=self.user.id))
+        self.assertEqual(200, response.status_code)
+        self.assertTrue("update", response.context["profile"])
+        # ユーザーページ上の投稿一覧
+        Article.objects.create(text="create1", author_id=self.user.id, created_at=timezone.now())
+        Article.objects.create(text="create2", author_id=self.user.id, created_at=timezone.now())
+        Article.objects.create(text="create3", author_id=self.user.id, created_at=timezone.now())
+        response = self.client.get(resolve_url('accounts:user_detail', pk=self.user.id))
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(3, response.context["my_article"].count())
 
     def test_UserUpdate(self):
-        response = self.client.get(resolve_url('accounts:user_update'))
+        # ユーザー情報作成
+        Profile.objects.create(introduction="create", sex=1, user_id=self.user.id)
+        response = self.client.get(resolve_url('accounts:user_update', pk=self.user.id))
         self.assertEqual(200, response.status_code)
+        # アクセスの確認
+        response = self.client.get(resolve_url('accounts:user_update', pk=self.user.id))
+        self.assertEqual(200, response.status_code)
+        # ユーザー情報更新
+        profile = Profile.objects.get(user_id=self.user.id)
+        profile.introduction = "update"
+        profile.save()
+        response = self.client.get(resolve_url('accounts:user_update', pk=self.user.id))
+        self.assertEqual(200, response.status_code)
+        self.assertTrue(response.context["profile"])
 
     def test_UserCreate(self):
-        response = self.client.get(resolve_url('accounts:user_create'))
+        # アクセスの確認
+        response = self.client.get(resolve_url('accounts:user_create', pk=self.user.id))
         self.assertEqual(200, response.status_code)
 
     def test_PasswordChange(self):
-        response = self.client.get(resolve_url('accounts:password_change'))
+        # アクセスの確認
+        response = self.client.get(resolve_url('accounts:password_change', pk=self.user.id))
         self.assertEqual(200, response.status_code)
+
+    def test_MyLikeArticle(self):
+        # アクセスの確認
+        response = self.client.get(resolve_url('accounts:mylike', pk=self.user.id))
+        self.assertEqual(200, response.status_code)
+        # いいねを追加し、追加した分がcontextに渡されているか
+        article = Article.objects.create(text="Do you like?", author_id=11, created_at=timezone.now())
+        article_id = article.id
+        Like.objects.create(article_id=article_id, user_id=self.user.id, created_at=timezone.now())
+        response = self.client.get(resolve_url('accounts:mylike', pk=self.user.id))
+        self.assertEqual(200, response.status_code)
+        self.assertTrue(1, len(response.context["like_list"]))
--- app/tests.py
+++ app/tests.py
@@ -1,10 +1,44 @@
 from django.test import TestCase
 from django.shortcuts import resolve_url
 from django.contrib.auth.models import User
+from .models import Article, Profile, Like
+from django.utils import timezone
+
+
+# モデルテスト
+class ArticleTest(TestCase):
+    def test___str__(self):
+        article = Article.objects.create(text="test", author_id=1, created_at=timezone.now())
+        self.assertTrue(article)
+        self.assertEqual("test", article.text)
+
+
+# モデルテスト
+class ProfileTest(TestCase):
+    def test___str__(self):
+        profile = Profile.objects.create(introduction="test", sex=1, user_id=1)
+        self.assertTrue(profile)
+        self.assertEqual("test", profile.introduction)
+
+
+def create_article(text, author_id):
+    return Article.objects.create(text=text, author_id=author_id, created_at=timezone.now())
+
+
+def update_article(text, author_id, update_text):
+    article = Article.objects.get(text=text, author_id=author_id)
+    article.text = update_text
+    return article.save()
+
+
+def delete_article(text, author_id):
+    article = Article.objects.get(text=text, author_id=author_id)
+    return article.delete()
 
 
 class ViewTest(TestCase):
 
+    # ログイン設定
     def setUp(self):
         self.user = User.objects.create_user(username='TestUser', password='passoworld')
         self.client.login(username='TestUser', password='passoworld')
@@ -14,18 +48,61 @@ class ViewTest(TestCase):
         self.assertEqual(200, response.status_code)
 
     def test_IndexView(self):
+        # アクセスと、何もデータが無いか
+        response = self.client.get(resolve_url('app:index'))
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(0, response.context['article_list'].count())
+        # データを一つ追加し、レスポンスが１つ増えているか
+        create_article("create", self.user.id)
+        response = self.client.get(resolve_url('app:index'))
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(1, response.context['article_list'].count())
+        # データを一つ削除し、レスポンスが０になっているか
+        delete_article("create", self.user.id)
         response = self.client.get(resolve_url('app:index'))
         self.assertEqual(200, response.status_code)
         self.assertEqual(0, response.context['article_list'].count())
 
     def test_CreateView(self):
-        response = self.client.get(resolve_url('app:index'))
+        # アクセスの確認
+        response = self.client.get(resolve_url('app:create'))
         self.assertEqual(200, response.status_code)
+        # データを追加できているか
+        create_article("create", self.user.id)
+        self.assertEqual(200, response.status_code)
+        self.assertTrue(Article.objects.get(text="create", author_id=self.user.id))
 
     def test_UpdateView(self):
+        # アクセスの確認
         response = self.client.get(resolve_url('app:index'))
         self.assertEqual(200, response.status_code)
+        # データを追加できているか
+        create_article("create", self.user.id)
+        self.assertEqual(200, response.status_code)
+        self.assertTrue(Article.objects.get(text="create", author_id=self.user.id))
+        # データを編集出来ているか
+        update_article("create", self.user.id, "update")
+        self.assertEqual(200, response.status_code)
+        self.assertTrue(Article.objects.get(text="update"))
 
     def test_DeleteView(self):
+        # アクセスの確認
         response = self.client.get(resolve_url('app:index'))
         self.assertEqual(200, response.status_code)
+        # データを追加できているか
+        create_article("create", self.user.id)
+        self.assertEqual(200, response.status_code)
+        self.assertTrue(Article.objects.get(text="create", author_id=self.user.id))
+        # データを削除できているか
+        delete_article("create", self.user.id)
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(0, Article.objects.all().count())
+
+    def test_like(self):
+        # データを一つ追加し、いいね確認
+        article = Article.objects.create(text="Do you like?", author_id=11, created_at=timezone.now())
+        article_id = article.id
+        Like.objects.create(article_id=article_id, user_id=self.user.id, created_at=timezone.now())
+        response = self.client.get(resolve_url('app:index'))
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(1, response.context['likes'][1])
